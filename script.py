23b53595144d2117d89ccb6e1751dc1ac2478096
gustave ploix <gustaveploix.pro@gmail.com>
2022-07-01 12:43:03 +0200

    finished the script, ready to be sent

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 import sys
+import re
+
+from pathlib import Path
+
 import pandas as pd 
+
+
+finalArray = []
 
 readableFile = pd.read_excel("./marketing_score_may_22.xlsx")
 
@@ -13,14 +20,83 @@ dataInList = data.values.tolist()
 
 split_lists = [dataInList[x:x+500] for x in range(0, len(dataInList), 500)]
 
-print("input => " + str(len(split_lists[0])))
+
+
+def convertToXLSX(arrays) :
+    listForDataFrame = []
+
+    for i in range(0, len(arrays)) :
+        listForDataFrame += arrays[i]
+
+    frame = pd.DataFrame(data=listForDataFrame)
+
+
+    frame.to_excel(str(Path.home() / "Downloads/data.xlsx"), index=False, header=True)
+
+
+def removeOccurenceInMSD(data) :
+    opens = []
+    clicks = []
+    softBounce = []
+    hardBounce = []
+    unsubscribed = []
+
+    lastOpen = ""
+    lastClick = ""
+    lastSoftBounce = ""
+    lastHardBounce = ""
+    lastUnsubscribed = ""
+
+    finalString = ""
+
+    cleanData = []
+
+    for j in range(0, len(data)) :
+        if len(data[j]) > 1 :
+            cleanData.append(data[j])
+
+    for i in range(0, len(cleanData)) :
+        if "open(s)" in cleanData[i] :
+            opens.append(cleanData[i] + "; ")
+            lastOpen = opens.pop()
+            # finalString += lastOpen + "; "
+        if "link click(s)" in cleanData[i] :
+            clicks.append(cleanData[i] + "; ")
+            lastClick = clicks.pop()
+            # finalString += lastClick + "; "
+        if "Soft Bounce" in cleanData[i] :
+            softBounce.append(cleanData[i] + "; ")
+            lastSoftBounce = softBounce.pop()
+            # finalString += lastSoftBounce + "; "
+        if "Hard Bounce" in cleanData[i] :
+            hardBounce.append(cleanData[i] + "; ")
+            lastHardBounce = hardBounce.pop()
+            # finalString += lastHardBounce + "; "
+        if "Unsubscribed" in cleanData[i] :
+            unsubscribed.append(cleanData[i] + "; ")
+            lastUnsubscribed = unsubscribed.pop()
+
+    finalString = lastOpen + lastClick + lastSoftBounce + lastHardBounce + lastUnsubscribed
+
+    return finalString
+
+
+
+
+
+
 
 def removeOccurences(data) : 
     
     cleanData = []
     indexToDelete = []
 
-    # print(data[0][0])
+    totalOpens = []
+    totalClicks = []
+
+    unsortedMarketingDetail = ""
+
+
 
     firstElem = {
         "email": data[0][0],
@@ -34,11 +110,53 @@ def removeOccurences(data) :
             indexToDelete.append(i)
 
             if "Soft bounce;" in str(data[i][2]) :
-                firstElem["marketing score detail"] += "Soft Bounce;"
+                unsortedMarketingDetail += "Soft Bounce;"
+            if "Hard bounce;" in str(data[i][2]) :
+                unsortedMarketingDetail += "Hard Bounce;"
+            if "open(s)" in str(data[i][2]):
+                sum = 0
+                opens = ""
+                splittedList = str(data[i][2]).split(";")
+                
+                for j in range(0, len(splittedList)) :
+                    if "open(s)" in splittedList[j] :
+                        opens = splittedList[j]
 
+                num = re.search(r'\d', opens).group()
+                totalOpens.append(int(num))
+                for k in range(0, len(totalOpens)) :
+                    sum += totalOpens[k]
+
+                finalString = str(sum) + " open(s);"
+                unsortedMarketingDetail += finalString
+
+            if "link click(s)" in str(data[i][2]):
+                sum = 0
+                clicks = ""
+                splittedList = data[i][2].split(";")
+
+                for j in range(0, len(splittedList)) :
+                    if "link click(s)" in splittedList[j] :
+                        clicks = splittedList[j]
+                
+
+                num = re.search(r'\d', clicks).group()
+                totalClicks.append(int(num))
+                for k in range(0, len(totalClicks)) :
+                    sum += totalClicks[k]
+                
+                finalString = str(sum) + " link click(s);"
+                
+                unsortedMarketingDetail += finalString
+
+            if "Unsubscribed" in str(data[i][2]):
+                unsortedMarketingDetail += "Unsubscribed;"
 
     for j in reversed(indexToDelete) :
         del(data[j])
+
+    tableOfMSD = unsortedMarketingDetail.split(";")
+    firstElem["marketing score detail"] = removeOccurenceInMSD(tableOfMSD)
 
     cleanData.append(firstElem)
 
@@ -48,18 +166,14 @@ def removeOccurences(data) :
         cleanData = cleanData + removeOccurences(data)
 
 
+    # if len(cleanData) > 10 :
+    #     convertToXLSX(cleanData)
+
     return cleanData
 
-half = round(len(split_lists) /2)
-print(split_lists[half])
-
-# print(split_lists[len(split_lists) - 1])
-
-# for i in range (0, half + 1) :
-#     print(removeOccurences(split_lists[i]))
-
 for i in range(0, len(split_lists)) :
-    print(removeOccurences(split_lists[i]))
+    # print(removeOccurences(split_lists[i]))
+    finalArray.append(removeOccurences(split_lists[i]))
 
-    # print(i)
-# removeOccurences(split_lists[0])
+if len(finalArray) > 10 :
+    convertToXLSX(finalArray)
